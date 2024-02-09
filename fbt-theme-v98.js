6d470d1ac1bfc9d0d0d0e64a48e6a79a5157afbd
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2024-02-09 09:22:53 +0530

    Update and rename fbt-theme-v97.js to fbt-theme-v98.js

--- fbt-theme-v98.js
+++ fbt-theme-v98.js
@@ -52,7 +52,8 @@ async function calculateTotalPrice(products, widgetElement,currency,totalPriceTe
           }else{
             discountAmount = formattedTotalPrice - discountValue
           }
-          totalPriceTextValue.innerHTML = totalPriceText + ': '+ `${currency + formattedTotalPrice} --- ${discountAmount.toFixed(2)}`;
+          let finalAmount = formattedTotalPrice - discountAmount
+          totalPriceTextValue.innerHTML = totalPriceText + ': '+ `${currency + finalAmount } --- ${formattedTotalPrice.toFixed(2)}`;
           totalPriceTextValue.style = "margin-top:10px";
           return true;
       }
