f94239ded4320adfacce7d43abd2e7237b680247
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2024-02-09 17:30:53 +0530

    Update and rename fbt-theme-v147.js to fbt-theme-v148.js

--- fbt-theme-v148.js
+++ fbt-theme-v148.js
@@ -197,7 +197,7 @@ function setTotalPrice(discountValue,discountType,formattedTotalPrice,totalPrice
           strikeSpan.style.textDecoration = 'line-through';
           strikeSpan.style.fontSize = '16px';
           strikeSpan.style.marginLeft = '10px';
-          totalPriceTextValue.innerHTML = `${totalPriceText}: ${currency}${finalAmount > 0 ? finalAmount : 0}`;
+          totalPriceTextValue.innerHTML = `${totalPriceText}: ${currency}${finalAmount > 0 ? finalAmount.toFixed(2) : 0}`;
 
           totalPriceTextValue.style = "margin-top:10px";
           totalPriceTextValue.appendChild(strikeSpan);
