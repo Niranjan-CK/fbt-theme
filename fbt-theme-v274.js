e825b8837c237b3ffffaf355d2f6fdb092fae358
Niranjan C K <135952156+Niranjan-CK@users.noreply.github.com>
2024-02-19 12:12:30 +0530

    Update and rename fbt-theme-v273.js to fbt-theme-v274.js

--- fbt-theme-v274.js
+++ fbt-theme-v274.js
@@ -15,6 +15,7 @@ async function calculateTotalPrice(products, widgetElement,currency,totalPriceTe
               let productId = productItem.getAttribute("data-product-id");
               let product = productRef.find((product) => Number(product.id) === Number(productId));
               let selectedIndex = (_a = productItem === null || productItem === void 0 ? void 0 : productItem.querySelector(".sf-product-variants-dropdown")) === null || _a === void 0 ? void 0 : _a.selectedIndex;
+              console.log(selectedIndex,'selectedIndex')
               let price = product?.price
               if (price){
                 totalPrice += price;
